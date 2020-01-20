32910cf44305187798be7aae0f0a0761e3ee0ef6
yuma11 <11yuma20@gmail.com>
2020-01-20 14:42:52 +0900

    first_commit

---
+++ CalcApp/Model/Car.swift
@@ -0,0 +1,13 @@
+//
+//  Car.swift
+//  CalcApp
+//
+//  Created by yuma on 2020/01/20.
+//  Copyright © 2020 Yuma. All rights reserved.
+//
+
+import Foundation
+
+class Car {
+    
+}
---
+++ CalcApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by yuma on 2020/01/20.
+//  Copyright © 2020 Yuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+
+}
+
